09822b7556beb9eaf32d367b60acd7db88a0031d
Rubén Díaz Fraga <rubendfraga@gmail.com>
2024-01-16 10:03:54 +0100

    Remove unnecessary files and styles

---
+++ cell03/ex00/background.js
@@ -0,0 +1,7 @@
+document.getElementById('changeColorButton').addEventListener('click', function() {
+    var red = Math.floor(Math.random() * 256);
+    var green = Math.floor(Math.random() * 256);
+    var blue = Math.floor(Math.random() * 256);
+    var randomColor = 'rgb(' + red + ', ' + green + ', ' + blue + ')';
+    document.body.style.backgroundColor = randomColor;
+});
