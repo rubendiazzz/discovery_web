38d92d39fc99c596d4f3b6b35be1e98181948999
Ruben Diaz <rdiaz-fr@student.42malaga.com>
2024-01-19 17:50:56 +0100

    Add directory creation functionality and update styles.css

--- cell00/ex05/build.sh
+++ cell00/ex05/build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]; then
-    echo "No arguments supplied"
-    exit 1
-fi
+# This script creates directories based on the command line arguments provided.
+# If no arguments are supplied, it displays an error message and exits with a non-zero status code.
 
-for arg in "$@"
-do
-   mkdir "ex$arg"
-done
+if [ $# -eq 0 ]; then # Check if the number of command line arguments is equal to 0
+	echo "No arguments supplied"  # Display an error message if no arguments are provided
+	exit 1  # Exit the script with a non-zero status code
+fi # End of the if statement
 
+for arg in "$@" # Iterate through the command line arguments
+do # Start of the for loop
+   mkdir "ex$arg"  # Create a directory with the name "ex" followed by the command line argument
+done # End of the for loop
